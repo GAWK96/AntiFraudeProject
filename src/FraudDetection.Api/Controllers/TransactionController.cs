e28c5fb28b62fc2361b2364c9ab30367aa895426
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-22 18:21:05 -0300

    Controller created

---
+++ src/FraudDetection.Api/Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using FraudDetection.Application.DTOs;
+using FraudDetection.Domain.Entities;
+using FraudDetection.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FraudDetection.Api.Controllers
+{
+	[ApiController]
+	[Route("transactions")]
+	public class TransactionController : ControllerBase
+	{
+		private readonly FraudDbContext _context;
+
+		public TransactionController(FraudDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] TransactionRequestDto request) 
+		{
+		   _context.Add(new Transaction
+		   {
+			   CustomerId = request.CustomerId,
+			   Amount = request.Amount,
+			   CreatedAt = DateTime.UtcNow
+		   });
+		   _context.SaveChanges();
+		   return Ok();
+		}
+	}
+}
