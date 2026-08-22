55780d3e1932f28135d5433efb7d2dcaaaf185e1
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-22 18:20:37 -0300

    Dtos created

---
+++ src/FraudDetection.Application/DTOs/TransactionRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FraudDetection.Application.DTOs
+{
+	public class TransactionRequestDto
+	{
+		public string CustomerId { get; set; }
+		public decimal Amount { get; set; }
+	}
+}
---
+++ src/FraudDetection.Application/DTOs/TransactionResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudDetection.Domain;
+
+namespace FraudDetection.Application.DTOs
+{
+	internal class TransactionResponseDto
+	{
+		public Guid Id { get; set; }
+		public string CustomerId { get; set; }
+		public decimal Amount { get; set; }
+		public TransactionStatus Status { get; set; }
+		public TransactionDecision? Decision { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
