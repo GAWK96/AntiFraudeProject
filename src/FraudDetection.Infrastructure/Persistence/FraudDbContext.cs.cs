1241ee23fc77700c43244515bbb7f80d48f9fe5f
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-22 15:58:24 -0300

    Context created on infrastructure

---
+++ src/FraudDetection.Infrastructure/Persistence/FraudDbContext.cs.cs
@@ -0,0 +1,15 @@
+﻿using FraudDetection.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FraudDetection.Infrastructure.Persistence
+{
+	public class FraudDbContext : DbContext
+	{
+	    public FraudDbContext(DbContextOptions<FraudDbContext> options) : base(options)
+		{
+		  
+		}
+
+		public DbSet<Transaction> Transactions => Set<Transaction>();
+	}
+}
