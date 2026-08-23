461c891715fbfc114688588be3afbca3fa818346
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-23 12:57:34 -0300

    Id added in transactionRequestDto

--- src/FraudDetection.Application/DTOs/TransactionRequestDto.cs
+++ src/FraudDetection.Application/DTOs/TransactionRequestDto.cs
@@ -6,6 +6,7 @@ namespace FraudDetection.Application.DTOs
 {
 	public class TransactionRequestDto
 	{
+		public int Id { get; set; }
 		public string CustomerId { get; set; }
 		public decimal Amount { get; set; }
 	}
