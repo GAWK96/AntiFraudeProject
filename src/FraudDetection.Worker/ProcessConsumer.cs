a1100227bf5ce7d0bbc736a5e467a5013a4e41de
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-23 12:58:14 -0300

    ProcessConsumer created. Settings in program.cs added

---
+++ src/FraudDetection.Worker/ProcessConsumer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using FraudDetection.Application.DTOs;
+using FraudDetection.Domain;
+using FraudDetection.Infrastructure.Persistence;
+using MassTransit;
+
+namespace FraudDetection.Worker
+{
+	internal sealed class ProcessConsumer : IConsumer<TransactionResponseDto>
+	{
+		private readonly ILogger<ProcessConsumer> _logger;
+		private readonly FraudDbContext _context;
+		public ProcessConsumer(ILogger<ProcessConsumer> logger, FraudDbContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
+		public async Task Consume(ConsumeContext<TransactionResponseDto> context)
+		{
+			_logger.LogInformation("Processando Transação Id:{Id}", context.Message.Id);
+			var transaction = _context.Transactions.FirstOrDefault(x => x.Id == context.Message.Id);
+			if (transaction != null)
+			{	transaction.Status = TransactionStatus.Processed;
+				await _context.SaveChangesAsync();
+				_logger.LogInformation("Transação Processada Id:{Id}", transaction.Id);
+			}
+		}
+	} 
+}
--- src/FraudDetection.Worker/Program.cs
+++ src/FraudDetection.Worker/Program.cs
@@ -1,7 +1,29 @@
+using FraudDetection.Infrastructure.Persistence;
 using FraudDetection.Worker;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddDbContext<FraudDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddMassTransit(x =>
+{
+	x.AddConsumer<ProcessConsumer>();
 
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.Host("amqp://localhost:5672", h =>
+		{
+			h.Username("guest");
+			h.Password("guest");
+		});
+
+		cfg.ReceiveEndpoint("process-transaction", e =>
+		{
+			e.ConfigureConsumer<ProcessConsumer>(context);
+		});
+	});
+});
 var host = builder.Build();
 host.Run();
