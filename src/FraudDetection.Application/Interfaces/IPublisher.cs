c600c5a9a565cdb423be35377b0009e8c9fe0866
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-24 15:11:06 -0300

    Publisher class and interface created

---
+++ src/FraudDetection.Application/Interfaces/IPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MassTransit;
+
+namespace FraudDetection.Application.Interfaces
+{
+	public interface IPublisher
+	{
+		Task PublishAsync<T>(T message, CancellationToken cancellationToken = default(CancellationToken)) where T : class;
+
+	}
+}
---
+++ src/FraudDetection.Infrastructure/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using FraudDetection.Application.Interfaces;
+using MassTransit;
+
+namespace FraudDetection.Infrastructure
+{
+	public class Publisher : IPublisher
+	{
+		private readonly IPublishEndpoint _publishEndpoint;
+
+		public Publisher(IPublishEndpoint publishEndpoint)
+		{
+			_publishEndpoint = publishEndpoint;
+		}
+		public Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : class
+		{
+			return _publishEndpoint.Publish(message, cancellationToken);
+		}
+	}
+}
