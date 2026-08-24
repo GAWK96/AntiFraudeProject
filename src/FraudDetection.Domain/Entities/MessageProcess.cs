0e91dbe2f64d63dd19596fe63224c54879151a3c
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-24 16:39:29 -0300

    Message process table added (email word is wrong)

---
+++ src/FraudDetection.Domain/Entities/MessageProcess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FraudDetection.Domain.Entities
+{
+	public class MessageProcess
+	{
+		public int Id { get; set; }
+		public int TransactionId { get; set; }
+		public DateTime ProcessedAt { get; set; }
+	}
+}
---
+++ src/FraudDetection.Infrastructure/Migrations/20260824183228_AddEmailTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FraudDetection.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageProcess",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TransactionId = table.Column<int>(type: "int", nullable: false),
+                    ProcessedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageProcess", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageProcess");
+        }
+    }
+}
