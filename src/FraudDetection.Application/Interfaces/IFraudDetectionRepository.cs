fbab3635be4e9ae9b1736b789f64e769f0d32a3d
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-26 18:18:31 -0300

    Interfaces for repository and service created

---
+++ src/FraudDetection.Application/Interfaces/IFraudDetectionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudDetection.Application.DTOs;
+using FraudDetection.Domain;
+using FraudDetection.Domain.Entities;
+
+namespace FraudDetection.Application.Interfaces
+{
+	public interface IFraudDetectionRepository
+	{
+		public TransactionModel GetTransactionById(int id);
+		public TransactionModel? GetTransactionByIdempotencyKey(Guid idempotencyKey);
+		public MessageModel GetMessageByMessageKey(Guid messageKey);
+		public Task<MessageModel> AddMessage(MessageModel message);
+
+		public Task<TransactionModel> AddTransaction(TransactionModel transaction);
+
+		public TransactionDecision SetTransactionDecisionAndStatus(TransactionModel transaction);
+	}
+}
---
+++ src/FraudDetection.Application/Interfaces/IFraudDetectionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudDetection.Application.DTOs;
+using FraudDetection.Domain;
+using FraudDetection.Domain.Entities;
+
+namespace FraudDetection.Application.Interfaces
+{
+	public interface IFraudDetectionService
+	{
+		public TransactionResponseDto? GetTransactionById(int id);
+		public MessageProcessDto? GetMessageByKey(Guid messageKey);
+		Task<TransactionResponseDto> AddTransactionAndPublish(TransactionRequestDto transaction);
+		Task<MessageProcessDto> AddMessage(MessageProcessDto message);
+		public TransactionResponseDto? GetTransactionByIdempotencyKey(Guid idempotencyKey);
+		public Task<TransactionDecision> SetTransactionDecisionAndStatus(TransactionResponseDto transaction);
+
+		Task<TransactionDecision> ProcessTransaction(TransactionResponseDto transaction, Guid messageKey);
+	}
+}
