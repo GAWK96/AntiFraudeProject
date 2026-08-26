9057ff4f67b335c5c6c58bb7758fbcea6e2b5b15
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-26 18:19:09 -0300

    Models (transaction and message) changed

---
+++ src/FraudDetection.Domain/Entities/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FraudDetection.Domain.Entities
+{
+	public class MessageModel
+	{
+		public int Id { get; set; }
+		public int TransactionId { get; set; }
+		public DateTime ProcessedAt { get; set; }
+		public Guid MessageKey { get; set; }
+	}
+}
---
+++ src/FraudDetection.Domain/Entities/TransactionModel.cs
@@ -0,0 +1,16 @@
+﻿namespace FraudDetection.Domain.Entities;
+
+public class TransactionModel
+{
+public int Id { get; set; }
+
+public required string CustomerId { get; set; }
+public decimal Amount { get; set; }
+
+public TransactionStatus Status { get; set; }
+
+public TransactionDecision? Decision { get; set; }
+
+public Guid IdempotencyKey { get; set; }
+public DateTime CreatedAt { get; set; }
+}
