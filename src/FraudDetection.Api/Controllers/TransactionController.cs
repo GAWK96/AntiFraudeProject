d817b08f9edd3db6da83f6bf50dc1503a75715e0
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-26 18:20:26 -0300

    refactor: controller changed. Context reference changed and service injected

--- src/FraudDetection.Api/Controllers/TransactionController.cs
+++ src/FraudDetection.Api/Controllers/TransactionController.cs
@@ -1,5 +1,6 @@
 ﻿using FraudDetection.Application.DTOs;
 using FraudDetection.Application.Interfaces;
+using FraudDetection.Application.Services;
 using FraudDetection.Domain;
 using FraudDetection.Domain.Entities;
 using FraudDetection.Infrastructure.Persistence;
@@ -12,62 +13,38 @@ namespace FraudDetection.Api.Controllers
 	[Route("transactions")]
 	public class TransactionController : ControllerBase
 	{
-		private readonly FraudDbContext _context;
 		private readonly ILogger<TransactionController> _logger;
+		private readonly IFraudDetectionService _service;
 
-		public TransactionController(FraudDbContext context, ILogger<TransactionController> logger)
+		public TransactionController(ILogger<TransactionController> logger, IFraudDetectionService service)
 		{
-			_context = context;
 			_logger = logger;
+			_service = service;
 		}
 
 		[HttpPost]
 		public async Task<IActionResult> Create([FromBody] TransactionRequestDto request, IPublisher bus)
 		{
 			_logger.LogInformation("Criando transação Id:{Id}", request.IdempotencyKey);
-			var checkTransaction = _context.Transactions.FirstOrDefault(x => x.IdempotencyKey == request.IdempotencyKey);
+			var checkTransaction = _service.GetTransactionByIdempotencyKey(request.IdempotencyKey);
 
 			if (checkTransaction != null)
 			{
 				return Ok(checkTransaction);
 			}
-			var transaction = new Transaction
+			else
 			{
-				CustomerId = request.CustomerId,
-				Amount = request.Amount,
-				CreatedAt = DateTime.UtcNow,
-				IdempotencyKey = request.IdempotencyKey
-			};
-			_context.Add(transaction);
-			_context.SaveChanges();
-			_logger.LogInformation("Transação criada Id:{Id}", request.IdempotencyKey);
-			var getTransaction = _context.Transactions.FirstOrDefault(x => x.Id == transaction.Id);
-			await bus.PublishAsync(new TransactionResponseDto
-			{
-				Id = getTransaction.Id,
-				CustomerId = getTransaction.CustomerId,
-				Amount = getTransaction.Amount,
-				CreatedAt = DateTime.UtcNow,
-				MessageKey = Guid.NewGuid(),
-			});
-			_logger.LogInformation("Mensagem publicada");
-			return Ok(getTransaction);
+				{
+					await _service.AddTransactionAndPublish(request);
+					return CreatedAtAction(nameof(GetTransaction), checkTransaction);
+				}
+			}
 		}
 
 		[HttpGet]
 		public IActionResult GetTransaction(int id)
 		{
-			var transaction = _context.Transactions
-								 .Where(x => x.Id == id)
-								 .Select(x => new TransactionResponseDto
-								 {
-									 Id = x.Id,
-									 CustomerId = x.CustomerId,
-									 Amount = x.Amount,
-									 Status = x.Status,
-									 Decision = x.Decision,
-									 CreatedAt = x.CreatedAt
-								 });
+			var transaction = _service.GetTransactionById(id);
 			return Ok(transaction);
 		}
 	}
