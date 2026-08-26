58f10ab81d951ffcf29299f8634ccfbbd7540a34
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-26 18:18:46 -0300

    Unit of work created

---
+++ src/FraudDetection.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudDetection.Application.Interfaces;
+using FraudDetection.Infrastructure.Persistence;
+
+namespace FraudDetection.Infrastructure
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly FraudDbContext _context;
+
+		public UnitOfWork(FraudDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task SaveChangesAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+	}
+}
