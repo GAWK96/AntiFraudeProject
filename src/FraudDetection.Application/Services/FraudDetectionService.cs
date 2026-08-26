42424fbceb82858df41466a2aefd032e42acd2fd
GAWK96 <guilherme.krieger@yahoo.com.br>
2026-08-26 18:19:27 -0300

    Service and repository created

---
+++ src/FraudDetection.Application/Services/FraudDetectionService.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using FraudDetection.Application.DTOs;
+using FraudDetection.Application.Interfaces;
+using FraudDetection.Domain;
+using FraudDetection.Domain.Entities;
+using MassTransit;
+using MassTransit.Transports.Fabric;
+using Microsoft.Extensions.Logging;
+using TransactionStatus = FraudDetection.Domain.TransactionStatus;
+
+namespace FraudDetection.Application.Services
+{
+	public class FraudDetectionService : IFraudDetectionService
+	{
+		private readonly IFraudDetectionRepository _repository;
+		private readonly IPublisher _bus;
+		private readonly ILogger<IFraudDetectionService> _logger;
+
+		private readonly IFraudMetrics _metrics;
+
+		private readonly IUnitOfWork _unitOfWork;
+		public FraudDetectionService(IFraudDetectionRepository repository, IPublisher bus, ILogger<IFraudDetectionService> logger, IUnitOfWork unitOfWork)
+		{
+			_repository = repository;
+			_bus = bus;
+			_logger = logger;
+			_unitOfWork = unitOfWork;
+		}
+
+		public TransactionResponseDto? GetTransactionById(int id)
+		{
+			var getTransaction = _repository.GetTransactionById(id);
+			if (getTransaction == null)
+			{
+				return null;
+			}	
+			var transactionMapped = new TransactionResponseDto
+			{
+				Id = getTransaction.Id,
+				CustomerId = getTransaction.CustomerId,
+				Amount = getTransaction.Amount,
+				Status = getTransaction.Status,
+				Decision = getTransaction.Decision,
+				CreatedAt = getTransaction.CreatedAt,
+				MessageKey = Guid.NewGuid(),
+			};
+			return transactionMapped;
+		}
+
+		public TransactionResponseDto? GetTransactionByIdempotencyKey(Guid idempotencyKey)
+		{
+			var getTransaction = _repository.GetTransactionByIdempotencyKey(idempotencyKey);
+			if (getTransaction == null)
+			{
+				return null;
+			}
+			var transactionMapped = new TransactionResponseDto
+			{
+				Id = getTransaction.Id,
+				CustomerId = getTransaction.CustomerId,
+				Amount = getTransaction.Amount,
+				Status = getTransaction.Status,
+				Decision = getTransaction.Decision,
+				CreatedAt = getTransaction.CreatedAt,
+				MessageKey = Guid.NewGuid(),
+			};
+			return transactionMapped;
+		}
+
+		public MessageProcessDto? GetMessageByKey(Guid messageKey)
+		{
+			var message = _repository.GetMessageByMessageKey(messageKey);
+			if (message == null)
+			{
+				return null;
+			}
+			var messageMapped = new MessageProcessDto
+			{
+				Id = message.Id,
+				TransactionId = message.TransactionId,
+				ProcessedAt = message.ProcessedAt,
+				MessageKey = message.MessageKey,
+			};
+			return messageMapped;
+		}
+
+		public async Task<TransactionResponseDto> AddTransactionAndPublish(TransactionRequestDto transaction)
+		{
+			var transactionMapped = new TransactionModel
+			{
+				CustomerId = transaction.CustomerId,
+				Amount = transaction.Amount,
+				CreatedAt = DateTime.UtcNow,
+				IdempotencyKey = transaction.IdempotencyKey,
+				Status = TransactionStatus.Pending
+			};
+			var transactionAdded = await _repository.AddTransaction(transactionMapped);
+			await _unitOfWork.SaveChangesAsync();
+			_logger.LogInformation("Transação criada Id:{Id}", transactionAdded.IdempotencyKey);
+			var getTransaction = _repository.GetTransactionByIdempotencyKey(transactionAdded.IdempotencyKey);
+			var message = new TransactionResponseDto
+			{
+				Id = getTransaction.Id,
+				CustomerId = transactionAdded.CustomerId,
+				Amount = transactionAdded.Amount,
+				CreatedAt = DateTime.UtcNow,
+				MessageKey = Guid.NewGuid(),
+			};
+			await _unitOfWork.SaveChangesAsync();
+			await _bus.PublishAsync(message);
+			_logger.LogInformation("Mensagem publicada");
+			return message;
+		}
+
+		public async Task<MessageProcessDto> AddMessage(MessageProcessDto message)
+		{
+			var messageMapped = new MessageModel
+			{
+				TransactionId = message.TransactionId,
+				ProcessedAt = DateTime.UtcNow,
+				MessageKey = message.MessageKey
+			};
+			await _repository.AddMessage(messageMapped);
+			return message;
+		}
+
+		public async Task<TransactionDecision> SetTransactionDecisionAndStatus(TransactionResponseDto transaction)
+		{
+		var getTransaction = _repository.GetTransactionById(transaction.Id);
+			getTransaction.Status = TransactionStatus.Processing;
+			await _unitOfWork.SaveChangesAsync();
+			var transactionProcessed = _repository.SetTransactionDecisionAndStatus(getTransaction);
+			return transactionProcessed;
+		}
+
+		public async Task<TransactionDecision> ProcessTransaction(TransactionResponseDto transaction,Guid messageKey)
+		{
+			try
+			{
+				_logger.LogInformation("Transação sendo processada Id:{Id}", transaction.Id);
+				var decision = await SetTransactionDecisionAndStatus(transaction);
+
+				var message = new MessageModel
+				{
+					TransactionId = transaction.Id,
+					MessageKey = messageKey,
+					ProcessedAt = DateTime.UtcNow
+				};
+
+				await _repository.AddMessage(message);
+
+				await _unitOfWork.SaveChangesAsync();
+
+				return decision;
+			}
+			catch (Exception ex)
+			{
+				_metrics.ProcessingError();
+
+				_logger.LogError(
+					ex,
+					"Erro ao processar transação Id:{Id}",
+					transaction.Id);
+
+				throw;
+			}
+		}
+
+	}
+}
---
+++ src/FraudDetection.Infrastructure/Repository/FraudDetectionRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FraudDetection.Application.DTOs;
+using FraudDetection.Application.Interfaces;
+using FraudDetection.Domain;
+using FraudDetection.Domain.Entities;
+using FraudDetection.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace FraudDetection.Infrastructure.Repository
+{
+	public class FraudDetectionRepository : IFraudDetectionRepository
+	{
+		private readonly FraudDbContext _context;
+
+		public FraudDetectionRepository(FraudDbContext context)
+		{
+			_context = context;
+		}
+
+		public TransactionModel? GetTransactionById(int id)
+		{
+			var transaction = _context.Transactions.FirstOrDefault(x => x.Id == id);
+			return transaction;
+		}
+
+		public TransactionModel? GetTransactionByIdempotencyKey(Guid idempotencyKey)
+		{
+			var transaction = _context.Transactions.FirstOrDefault(x => x.IdempotencyKey == idempotencyKey);
+			return transaction;
+		}
+
+		public MessageModel GetMessageByMessageKey(Guid messageKey)
+		{
+			var message = _context.MessageModel.FirstOrDefault(x => x.MessageKey == messageKey);
+			return message;
+		}
+
+		public async Task<MessageModel> AddMessage(MessageModel message)
+		{
+			_context.MessageModel.Add(message);
+			return message;
+		}
+
+		public async Task<TransactionModel> AddTransaction(TransactionModel transaction)
+		{
+			_context.Transactions.Add(transaction);
+			return transaction;
+		}
+
+		public TransactionDecision SetTransactionDecisionAndStatus(TransactionModel transaction)
+		{
+		var getTransaction = GetTransactionById(transaction.Id);
+			TransactionDecision decision;
+			if (getTransaction.Amount <= 1000)
+			{
+				decision = TransactionDecision.Approved;
+			}
+			else if (getTransaction.Amount > 1000 && getTransaction.Amount <= 5000)
+			{
+				decision = TransactionDecision.Review;
+			}
+			else 
+			{
+				decision = TransactionDecision.Rejected;
+			}
+			getTransaction.Status = TransactionStatus.Processed;
+			getTransaction.Decision = decision;
+			return decision;
+		}
+	}
+}
